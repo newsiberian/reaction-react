40a2de1d1bb3adbfc4a03f4db0a79327ee5f3e8a
newsiberian <newsiberian2015@yandex.ru>
2016-03-19 21:41:35 +0600

    added simple styles from SUI to new tag field;

--- imports/modules/products/components/productDetail/tags/ProductTagInputForm.jsx
+++ imports/modules/products/components/productDetail/tags/ProductTagInputForm.jsx
@@ -8,15 +8,70 @@ import Tag from "./Tag.jsx";
 const styles = StyleSheet.create({
   list: {
     listStyleType: "none"
+  },
+  input: {
+    borderRightColor: "transparent",
+    paddingTop: "0.678614em",
+    paddingBottom: "0.678614em",
+    paddingLeft: "2.67142857em",
+    paddingRight: "2.67142857em",
+    margin: 0,
+    maxWidth: "100%",
+    flex: "1 0 auto",
+    outline: 0,
+    textAlign: "left",
+    lineHeight: "1.2142em",
+    background: "#fff",
+    border: "1px solid rgba(34,36,38,.15)",
+    color: "rgba(0,0,0,.87)",
+    borderRadius: ".28571429rem",
+    transform: "box-shadow .1s ease,border-color .1s ease",
+    boxShadow: "none"
+  },
+  container: {
+    margin: 4,
+    position: "relative"
+  },
+  suggestionsContainer: {
+    borderColor: "#96c8da",
+    borderRadius: "0 0 0.285714rem 0.285714rem",
+    borderTopWidth: 0,
+    boxShadow: "0 2px 3px 0 rgba(34, 36, 38, 0.15)",
+    maxHeight: "20.2286rem",
+    minWidth: "calc(100% + 2px)",
+    outline: "0 none",
+    overflowX: "hidden",
+    overflowY: "auto",
+    transition: "opacity 0.1s ease 0s",
+    width: "calc(100% + 2px)",
+    left: 0,
+    background: "#fff none repeat scroll 0 0",
+    position: "absolute",
+    textAlign: "left"
+  },
+  suggestion: {
+    boxShadow: "none",
+    color: "rgba(0, 0, 0, 0.87)",
+    cursor: "pointer",
+    display: "block",
+    height: "auto",
+    lineHeight: "1em",
+    padding: "0.714286rem 1.14286rem",
+    position: "relative",
+    borderTop: "1px solid #fafafa",
+    ":hover": {
+      background: "rgba(0, 0, 0, 0.05) none repeat scroll 0 0",
+      color: "rgba(0, 0, 0, 0.95)"
+    }
   }
 });
 
 const theme = {
-  container: "react-autosuggest__container",
+  container: styles.container, // "react-autosuggest__container",
   containerOpen: "react-autosuggest__container--open",
-  input: "react-autosuggest__input",
-  suggestionsContainer: "react-autosuggest__suggestions-container",
-  suggestion: "react-autosuggest__suggestion",
+  input: styles.input, // "react-autosuggest__input",
+  suggestionsContainer: styles.suggestionsContainer, // "react-autosuggest__suggestions-container",
+  suggestion: styles.suggestion, // "react-autosuggest__suggestion",
   suggestionFocused: "react-autosuggest__suggestion--focused",
   sectionContainer: "react-autosuggest__section-container",
   sectionTitle: "react-autosuggest__section-title",
