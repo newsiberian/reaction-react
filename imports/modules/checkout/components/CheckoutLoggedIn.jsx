0368016e5ff93d5c76deff822dc65ae488e63adf
newsiberian <newsiberian2015@yandex.ru>
2016-04-11 12:17:36 +0600

    - fixed styles in `CheckoutLoggedIn`;

--- imports/modules/checkout/components/CheckoutLoggedIn.jsx
+++ imports/modules/checkout/components/CheckoutLoggedIn.jsx
@@ -3,10 +3,8 @@ import { translate } from "react-i18next/lib";
 
 const CheckoutLoggedIn = props => {
   return (
-    <div className="ui attached segment">
-      <h3 className="ui header" style={{ padding: "1rem" }}>
-        {props.t("checkoutLogin.welcome")}
-      </h3>
+    <div style={{ padding: "1rem 1rem 1rem 2rem" }}>
+      <h3>{props.t("checkoutLogin.welcome")}</h3>
     </div>
   );
 };
